9cb08bd85e17e776a79d56d2d8be644147f9128f
Athi <athisachin@gmail.com>
2024-10-02 10:43:09 +0530

    test1

---
+++ src/main/java/com/example/ProductServiceApllication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApllication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApllication.class, args);
+	}
+}
